d655ff6a4263538491dad0f7e4ccf7686b82bc3c
Jair <jairaceves56@gmail.com>
2022-12-12 17:46:06 -0600

    validacion de Funcionamiento de Geolocation

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+if ( !navigator.geolocation ){
+    alert('Tu navegador no soporta Geolocation')
+    throw new Error('Tu navegador no soporta Geolocation')
+}
+
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
